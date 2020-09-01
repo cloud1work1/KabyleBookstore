5912bca8fb8f8746444b09c6cfecaa5797c69283
AMukherjee9 <AMukherjee9@slb.com>
2020-09-01 11:13:59 +0530

    Initial commit

---
+++ src/main/java/in/kabyle/KabyleBookstoreApplication.java
@@ -0,0 +1,13 @@
+package in.kabyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabyleBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabyleBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/kabyle/KabyleBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.kabyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabyleBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
